49e211d6a72fd4e38f2e9e30afb5a48d35e4b22e
Nikolay.B <nikolbar@yandex.ru>
2022-12-17 17:47:35 +0300

    initial commit

---
+++ Lec_01/lec1_1.py
@@ -0,0 +1,13 @@
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# else:
+#     print('original = ', original, ', Пожалуй')
+#     print('\bхватит )')
+# print(inverted)
+
+# a, b, c = 1, 2, 3
+# # a: 1 b: 2 c: 3
+# print('a: {2} b: {1} c: {0}'.format(a, b, c))
