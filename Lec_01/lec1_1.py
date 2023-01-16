44364e9b992307b6b838d8dc7b6eee00b1ac9829
Nikolay.B <nikolbar@yandex.ru>
2023-01-17 02:45:40 +0300

    задача 2 реш.

--- Lec_01/lec1_1.py
+++ Lec_01/lec1_1.py
@@ -1,13 +1,37 @@
-# original = 23
-# inverted = 0
-# while original != 0:
-#     inverted = inverted * 10 + (original % 10)
-#     original //= 10
-# else:
-#     print('original = ', original, ', Пожалуй')
-#     print('хватит )')
-# print(inverted)
+import os
+import random as r
+os.system('clear')
 
-# a, b, c = 1, 2, 3
-# # a: 1 b: 2 c: 3
-# print('a: {2} b: {1} c: {0}'.format(a, b, c))
+
+# def changeIndexTuple(kr, indx, num):
+#     lst = list(kr)
+#     lst[indx] = num
+#     kr = tuple(lst)
+#     return kr
+
+
+# kr = tuple(r.randint(0, 25) for i in range(10))
+# indx, val = 3, 88
+
+# result = changeIndexTuple(kr, indx, val)
+# print(result)
+
+# '''Красивые: Илья Федор Семен Олег Лев Антон Артем Боря Стас Марк Ян
+# Умные: 'Илья', 'Георгий', 'Лев', 'Демьян', 'Антон', 'Владислав', 'Боря', 'Стас', 'Марк', 'Влад'
+# Сильные: 'Федор', 'Георгий', 'Олег', 'Демьян', 'Артем', 'Елисей', 'Боря', 'Стас', 'Влад' '''
+
+# beauty = set(['Илья', 'Федор', 'Семен', 'Олег', 'Лев',
+#               'Антон', 'Артем', 'Боря', 'Стас', 'Марк', 'Ян'])
+# strong = set(['Илья', 'Георгий', 'Лев', 'Демьян', 'Антон',
+#               'Владислав', 'Боря', 'Стас', 'Марк', 'Влад'])
+# smart = set(['Федор', 'Георгий', 'Олег', 'Демьян',
+#             'Артем', 'Елисей', 'Боря', 'Стас', 'Влад'])
+
+# result = smart.intersection(strong.intersection(beauty))
+# print(result)
+rand_num_set = []
+for _ in range(10):
+    rand_num_set.append(r.randint(0, 20))
+print(rand_num_set)
+rand_num_set = set(rand_num_set)
+print(rand_num_set)
--- Sem_03/Sem_03.py
+++ Sem_03/Sem_03.py
@@ -5,22 +5,37 @@ os.system('clear')
 def task01():
     '''Задача 1. Создайте файл. Запишите в него N первых элементов 
     последовательности Фибоначчи.  6 -> 1 1 2 3 5 8'''
-    n = 6
+    n = 8
     num_fib = ''
     lst = [1, 1]
     for i in range(2, n):
         lst.append(lst[-1] + lst[i-2])
+
     for s in lst:
         num_fib += str(s)+' '
+
     data = open("task01.txt", mode="w", encoding='utf-8')
-    data.write(num_fib)
+    data.writelines(num_fib)
     data.close
 
 
 def task02():
     '''Задача 2. В файле находятся названия фруктов. Выведите все фрукты, 
     названия которых начинаются на заданную букву. а -> абрикос, авокадо, апельсин, айва.'''
-    print()
+    symb_in = input('Введите начальную букву :')
+    if len(symb_in) > 1:
+        symb_in = symb_in[0]
+    data = open('task02.txt', mode='r', encoding='utf-8')
+    lst = data.readlines()
+    result = []
+    data.close
+    for current_word in lst:
+        if current_word[0].lower() == symb_in.lower():
+            result.append(current_word.rstrip('\n'))
+    if len(result) == 0:
+        print(f'Увы, на букву {symb_in} ничего не нашлось!')
+    else:
+        print(*result)
 
 
 def task03():
@@ -28,6 +43,6 @@ def task03():
     print()
 
 
-task01()
-# task02()
+# task01()
+task02()
 # task03()
