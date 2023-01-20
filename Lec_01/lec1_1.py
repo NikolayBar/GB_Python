8d694adc865b3abbffab09047cca3ea92a56b045
Nikolay.B <nikolbar@yandex.ru>
2023-01-20 14:18:55 +0300

    Start HW Sem_04

--- Lec_01/lec1_1.py
+++ Lec_01/lec1_1.py
@@ -3,35 +3,43 @@ import random as r
 os.system('clear')
 
 
-# def changeIndexTuple(kr, indx, num):
-#     lst = list(kr)
-#     lst[indx] = num
-#     kr = tuple(lst)
-#     return kr
+def task02():
+    data = open("Телефоны.txt", mode="r", encoding="utf-8")
+    phones = data.readlines()
+    data.close
+
+    shop01 = set(phones[1].replace('\n', '').split(', '))
+    shop02 = set(phones[3].removesuffix('\n').split(', '))
+    shop02 = set(phones[5].removesuffix('\n').split(', '))
+
+    print(*shop01)
 
 
-# kr = tuple(r.randint(0, 25) for i in range(10))
-# indx, val = 3, 88
+# def task03():
+#     '''Задача 2. В зоопарк отправили отчёт о новом поступлении зверей с ошибкой, в результате которой некоторые данные не расшифровались. Расшифруйте данные. Определите, в какой клетке находится лев. Номер клетки совпадает с номером строки. 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'''
+#     find = 'барсук'
+#     alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+#     dic = []
+#     result = []
+#     for i in range(len(alphabet)):
+#         x = str(bin(i))[2:].zfill(6)
+#         dic.append([x] + [alphabet[i]])
+#     dic = dict(dic)
+#     data = open("Животные.txt", mode="r", encoding="utf-8")
+#     lst = data.readlines()
+#     data.close
+#     lst = [s.replace('\n', '') for s in lst]
 
-# result = changeIndexTuple(kr, indx, val)
-# print(result)
+#     for el in lst:
+#         name = ''
+#         for i in range(0, len(el), 6):
+#             name = name + dic[el[i: i+6]]
+#         result.append(name)
 
-# '''Красивые: Илья Федор Семен Олег Лев Антон Артем Боря Стас Марк Ян
-# Умные: 'Илья', 'Георгий', 'Лев', 'Демьян', 'Антон', 'Владислав', 'Боря', 'Стас', 'Марк', 'Влад'
-# Сильные: 'Федор', 'Георгий', 'Олег', 'Демьян', 'Артем', 'Елисей', 'Боря', 'Стас', 'Влад' '''
+#     if find in result:
+#         print(f'{find} живет в клетке № {result.index(find)}')
+#     else:
+#         print(f'{find} не значится в списке')
 
-# beauty = set(['Илья', 'Федор', 'Семен', 'Олег', 'Лев',
-#               'Антон', 'Артем', 'Боря', 'Стас', 'Марк', 'Ян'])
-# strong = set(['Илья', 'Георгий', 'Лев', 'Демьян', 'Антон',
-#               'Владислав', 'Боря', 'Стас', 'Марк', 'Влад'])
-# smart = set(['Федор', 'Георгий', 'Олег', 'Демьян',
-#             'Артем', 'Елисей', 'Боря', 'Стас', 'Влад'])
 
-# result = smart.intersection(strong.intersection(beauty))
-# print(result)
-rand_num_set = []
-for _ in range(10):
-    rand_num_set.append(r.randint(0, 20))
-print(rand_num_set)
-rand_num_set = set(rand_num_set)
-print(rand_num_set)
+# task03()
