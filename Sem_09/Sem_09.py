8d07aa83b0c2d52c03881f4adaa8cb4897f0f9fe
NikolayBar <nikolbar@yandex.ru>
2023-02-03 23:12:33 +0300

    start Sem_09 HW

--- Sem_09/Sem_09.py
+++ Sem_09/Sem_09.py
@@ -1,1 +1,25 @@
 import telebot
+from random import randint as ri
+
+bot = telebot.TeleBot("TOKEN", parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет, как у тебя дела?")
+
+
+
+# Handles all text messages that contains the commands '/start' or '/help'.
+@bot.message_handler(commands=['rand'])
+def handle_start_help(message):
+    num = str(ri(10,22))
+    bot.reply_to(message, num)
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
+
+
